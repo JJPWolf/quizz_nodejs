b920516e098de0abfc3cc9d952949d3ae2ab6041
JJPWolf <58184908+JJPWolf@users.noreply.github.com>
2021-03-19 17:11:18 +0100

    rejoindre ok

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+/*
 let PORT = process.env.PORT || 9091;
 const http = require("http");
 
@@ -13,7 +13,8 @@ app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 
 app.get("/", (req,res)=> res.sendFile(__dirname + "/index.html"))
 
-app.listen(PORT, ()=>console.log("Listening on http port 9091"))
+app.listen(PORT, ()=>console.log("Listening on http port "+PORT))
+
 const websocketServer = require("websocket").server
 const httpServer = http.createServer();
 httpServer.listen(9090, () => console.log("Listening.. on 9090"))
@@ -24,13 +25,140 @@ const games = {};
 const wsServer = new websocketServer({
     "httpServer": httpServer
 })
-wsServer.on("request", request => {
+*/
+'use strict';
+
+const express = require('express');
+const { Server } = require('ws');
+const path = require('path')
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express();
+  server.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+  server.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+  server.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+  server.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  let httpServer = server.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+//hashmap clients
+const clients = {};
+const games = {};
+const wss = new Server({ server : httpServer });
+
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+    
+    ws.on('close', () => console.log('Client disconnected'));
+    ws.on("message", message => {
+            
+            console.log(message);
+            const result = JSON.parse(message)
+            console.log(result);
+            //I have received a message from the client
+            //a user want to create a new game
+            if (result.method === "create") {
+                const clientId = result.clientId;
+                const nbQuestions = result.nbQuestions;
+                const tpsQuestions = result.tpsQuestions;
+                const gameId = guid();
+                games[gameId] = {
+                    "id": gameId,
+                    "balls": 20,
+                    "nbQuestions" : nbQuestions,
+                    "tpsQuestions" : tpsQuestions,
+                    "clients": [
+                        {
+                            "clientId" : clientId,
+                            "pseudo" : clients[clientId].pseudo
+                        }
+                    ]
+                }
+    
+                const payLoad = {
+                    "method": "create",
+                    "game" : games[gameId]
+                }
+    
+                const con = clients[clientId].connection;
+                con.send(JSON.stringify(payLoad));
+            }
+    
+            if(result.method === "setPseudo"){
+                const clientId = result.clientId;
+                const pseudo = result.pseudo;
+                clients[clientId].pseudo = pseudo;
+            }
+    
+            //a client want to join
+            if (result.method === "join") {
+    
+                const clientId = result.clientId;
+                const gameId = result.gameId;
+                const game = games[gameId];
+                if (game.clients.length >= 3) 
+                {
+                    //sorry max players reach
+                    return;
+                }
+                const color =  {"0": "Red", "1": "Green", "2": "Blue"}[game.clients.length]
+                game.clients.push({
+                    "clientId": clientId,
+                    "color": color
+                })
+                //start the game
+                if (game.clients.length === 3) updateGameState();
+    
+                const payLoad = {
+                    "method": "join",
+                    "game": game
+                }
+                //loop through all clients and tell them that people has joined
+                game.clients.forEach(c => {
+                    clients[c.clientId].connection.send(JSON.stringify(payLoad))
+                })
+            }
+            //a user plays
+            if (result.method === "play") {
+                const gameId = result.gameId;
+                const ballId = result.ballId;
+                const color = result.color;
+                let state = games[gameId].state;
+                if (!state)
+                    state = {}
+                
+                state[ballId] = color;
+                games[gameId].state = state;
+                
+            }
+        
+
+    })
+    //generate a new clientId
+    const clientId = guid();
+    clients[clientId] = {
+        "connection":  ws,
+        "pseudo" : ""
+    }
+
+    const payLoad = {
+        "method": "connect",
+        "clientId": clientId
+    }
+    //send back the client connect
+    ws.send(JSON.stringify(payLoad))
+    
+});
+/*
+wss.on("request", request => {
+    console.log("Handling request from " + request.origin);
     //connect
     const connection = request.accept(null, request.origin);
     connection.on("open", () => console.log("opened!"))
     connection.on("close", () => console.log("closed!"))
     connection.on("message", message => {
         const result = JSON.parse(message.utf8Data)
+        console.log(result);
         //I have received a message from the client
         //a user want to create a new game
         if (result.method === "create") {
@@ -125,7 +253,7 @@ wsServer.on("request", request => {
     connection.send(JSON.stringify(payLoad))
 
 })
-
+*/
 
 function updateGameState(){
 
